d70804dad512d330fd12bc320910015a188bb0fc
azizbek <azizbekbootcam@gmail.com>
2021-07-20 08:21:30 +0500

    timeLine created

--- src/components/Profile/style.js
+++ src/components/Profile/style.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 // import myImage from "../../assets/images/user.jpg";
 export const ProfileShadow = styled.div`
-  background-color: whitesmoke;
   border-radius: 6px;
   width: 100%;
   display: inline-block;
@@ -9,8 +8,6 @@ export const ProfileShadow = styled.div`
 
 export const ProfileImg = styled.div`
   margin-top: -10px;
-  /* -webkit-clip-path: polygon(0.9%, 100% 0, 100% 94%, 0 100%);
-  clip-path: polygon(0.9%, 100% 0, 100% 94%, 0 100%); */
   img {
     width: 100%;
   }
