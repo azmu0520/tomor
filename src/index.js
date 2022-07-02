d391029c4e14b217e3141f8ddac40ea3af1ae316
azmu0520 <azizbekbootcam@gmail.com>
2022-07-02 12:23:11 +0500

    setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-
-ReactDOM.render(
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Root from './root';
+import { BrowserRouter as Router } from 'react-router-dom';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <Router>
+      <Root />
+    </Router>
+  </React.StrictMode>
 );
